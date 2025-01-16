5707d312f727785c5c32a3254cec831ad056763f
e-mellinandrichier <emmanuelle.mellinand-richier@laplateforme.io>
2025-01-16 11:09:15 +0100

    feat; parenthesis ok. test needed

--- my_calculator.py
+++ my_calculator.py
@@ -26,9 +26,11 @@ def division(my_operation, my_second_operation):
     result = my_operation / my_second_operation
     return result
 
-
-my_list = []
+# my_list = ["12", "x", "(", "2", "+", "(", "2", "x", "(", "4", "+", "8", ")", ")", ")", "="]
+# my_list = ["2", "+", "3", "x", "4", "+" ,"(", "1", "+", "(", "3", "+", "4", ")", ")", "="]
 user_truc = ""
+my_list = []
+index = 0
 
 while user_truc != "=":
     user_truc = input("entrez des trucs")
@@ -36,11 +38,10 @@ while user_truc != "=":
 
 
 
-print(my_list)
+# print(my_list)
 def truc(my_list):
     i = 0
     while i < len(my_list):
-        parenthesis(my_list, i)
         if my_list[i] == "x" or my_list[i] == ":":
             if my_list[i] == "x":
                 result = multiply(float(my_list[i-1]), float(my_list[i+1]))
@@ -72,14 +73,53 @@ def truc(my_list):
         i +=1
     return(my_list)
 
-def parenthesis(my_list, index):
+def parenthesis(my_list):
+    # print(my_list)
+    last = 0
     my_stuff = []
+    index = 0
     while index < len(my_list):
         if my_list[index] == "(":
-            index +=1
+            # print(my_list[index])
+            # print(index)
             while my_list[index] != ")":
-                my_stuff.append(my_list[index])
-                index += 1
+                counterp = my_list.count("(")
+                if my_list[index] == "(":
+                    last = index
+                index +=1
+            index +=1
+        else : index +=1
+    # print(counterp)
+    index = last +1
+    # print(my_list[index])
+    while my_list[index] != ")":
+        my_stuff.append(my_list[index])
         index +=1
-    print(my_stuff)
-haha = parenthesis(my_list)
+    truc(my_stuff)
+    # print(my_stuff)
+    index2 = last +1
+    my_list[last] = my_stuff[0]
+    # print(my_list)
+    while my_list[index2] != ")":
+        del my_list[index2]
+    del my_list[index2]
+    # print(my_list)
+    if counterp == 1:
+        truc(my_list)
+
+
+index = 0
+while index < len(my_list):
+    # print(index)
+    if my_list[index] == "(":
+        index +=1
+        while index < len(my_list):
+            # print("truc", index)
+            parenthesis(my_list)
+            index +=1
+    else : index +=1
+# print(len(my_list))
+truc(my_list)
+# print(my_list)
+del my_list[-1]
+print(my_list[0])
