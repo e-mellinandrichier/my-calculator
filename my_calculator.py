c18eee928fec855b00b454c68fbfb6e10932ad28
e-mellinandrichier <emmanuelle.mellinand-richier@laplateforme.io>
2025-01-20 09:45:37 +0100

    fix: cleaning the code

--- my_calculator.py
+++ my_calculator.py
@@ -1,13 +1,3 @@
-# my_operation = ""
-# my_number = ""
-
-# for i in my_operation:
-#     if ord(i) in range(48,57):
-#         my_number += i
-#     else : return "pas bon"
-
-# user_answer = ""
-
 result = 0
 
 def add(my_operation, my_second_operation):
@@ -26,19 +16,6 @@ def division(my_operation, my_second_operation):
     result = my_operation / my_second_operation
     return result
 
-# my_list = ["12", "x", "(", "2", "+", "(", "2", "x", "(", "4", "+", "8", ")", ")", ")", "="]
-# my_list = ["2", "+", "3", "x", "4", "+" ,"(", "1", "+", "(", "3", "+", "4", ")", ")", "="]
-user_truc = ""
-my_list = []
-index = 0
-
-while user_truc != "=":
-    user_truc = input("entrez des trucs")
-    my_list.append(user_truc)
-
-
-
-# print(my_list)
 def truc(my_list):
     i = 0
     while i < len(my_list):
@@ -74,14 +51,11 @@ def truc(my_list):
     return(my_list)
 
 def parenthesis(my_list):
-    # print(my_list)
     last = 0
     my_stuff = []
     index = 0
     while index < len(my_list):
         if my_list[index] == "(":
-            # print(my_list[index])
-            # print(index)
             while my_list[index] != ")":
                 counterp = my_list.count("(")
                 if my_list[index] == "(":
@@ -89,37 +63,40 @@ def parenthesis(my_list):
                 index +=1
             index +=1
         else : index +=1
-    # print(counterp)
     index = last +1
-    # print(my_list[index])
     while my_list[index] != ")":
         my_stuff.append(my_list[index])
         index +=1
     truc(my_stuff)
-    # print(my_stuff)
     index2 = last +1
     my_list[last] = my_stuff[0]
-    # print(my_list)
     while my_list[index2] != ")":
         del my_list[index2]
     del my_list[index2]
-    # print(my_list)
     if counterp == 1:
         truc(my_list)
 
+def menu():
+    user_truc = ""
+    my_list = []
+    index = 0
 
-index = 0
-while index < len(my_list):
-    # print(index)
-    if my_list[index] == "(":
-        index +=1
-        while index < len(my_list):
-            # print("truc", index)
-            parenthesis(my_list)
+    while user_truc != "=":
+        user_truc = input("Entrez un nombre ou un opérateur (+, -, x, : ou ())")
+        my_list.append(user_truc)
+    
+    my_history_list = my_list
+
+    index = 0
+    while index < len(my_list):
+        if my_list[index] == "(":
             index +=1
-    else : index +=1
-# print(len(my_list))
-truc(my_list)
-# print(my_list)
-del my_list[-1]
-print(my_list[0])
+            while index < len(my_list):
+                parenthesis(my_list)
+                index +=1
+        else : index +=1
+    truc(my_list)
+    del my_list[-1]
+    print(my_list[0])
+    menu()
+menu()
